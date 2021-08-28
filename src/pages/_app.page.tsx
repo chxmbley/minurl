23c1acb5598452a78a0bcdda610300df24830b2a
Joshua Chumbley <joshua.chumbley@strayfar.com>
2021-08-28 10:48:25 -0500

    Refactor for TypeScript

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,7 +1,7 @@
-import '../styles/globals.css'
+import type { FC } from 'react';
+import type { AppProps } from 'next/app';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const App: FC<AppProps> = ({ Component, pageProps }) => <Component {...pageProps} />;
 
-export default MyApp
+export default App;
--- src/pages/api/hello.api.ts
+++ src/pages/api/hello.api.ts
@@ -1,5 +1,7 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiHandler } from 'next';
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
+const handler: NextApiHandler = (req, res) => {
+  res.status(200).json({ name: 'John Doe' });
+};
+
+export default handler;
