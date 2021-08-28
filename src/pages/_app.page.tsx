5ab1c39892f39d7f32d2892b8d6ab94933df5785
Joshua Chumbley <jchumbley@pm.me>
2021-08-28 16:14:04 -0500

    Fix global CSS import syntax

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,6 +1,6 @@
 import type { FC } from 'react';
 import type { AppProps } from 'next/app';
-import '../styles/globals.css';
+import '~styles/globals.css';
 
 const App: FC<AppProps> = ({ Component, pageProps }) => <Component {...pageProps} />;
 
