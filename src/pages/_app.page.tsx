fe031c64d76c8ae4f4762202609b812974812d8d
Joshua Chumbley <jchumbley@pm.me>
2021-09-06 14:43:07 -0500

    Add Tailwind to global CSS

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,6 +1,6 @@
 import type { FC } from 'react';
 import type { AppProps } from 'next/app';
-import '~styles/globals.css';
+import '~styles/globals.scss';
 
 const App: FC<AppProps> = ({ Component, pageProps }) => <Component {...pageProps} />;
 
