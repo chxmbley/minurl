f1a12ef534d1c791153c584a445068b0ef8407e0
Joshua Chumbley <jchumbley@pm.me>
2021-12-29 10:03:48 -0600

    Set up GraphQL API client

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,7 +1,13 @@
+import { ApolloProvider } from '@apollo/client';
+import client from '~lib/api/client';
 import type { FC } from 'react';
 import type { AppProps } from 'next/app';
 import '~styles/globals.scss';
 
-const App: FC<AppProps> = ({ Component, pageProps }) => <Component {...pageProps} />;
+const App: FC<AppProps> = ({ Component, pageProps }) => (
+  <ApolloProvider client={client}>
+    <Component {...pageProps} />
+  </ApolloProvider>
+);
 
 export default App;
