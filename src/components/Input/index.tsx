b3e5c95b0b9c6696c130feff659cdf20380ae5a6
Joshua Chumbley <jchumbley@protonmail.com>
2022-01-16 14:22:45 -0600

    Separate Input component styling

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,20 +1,12 @@
 import cn from 'classnames';
 import { forwardRef, HTMLProps } from 'react';
 import type { ForwardRefRenderFunction } from 'react';
+import styles from './Input.module.scss';
 
 /** Styled input component */
 const Input: ForwardRefRenderFunction<HTMLInputElement, HTMLProps<HTMLInputElement>> = (
   { className, ...props },
   ref,
-) => (
-  <input
-    ref={ref}
-    className={cn(
-      className,
-      'border border-slate-200 dark:border-slate-700 px-6 h-12 rounded-full shadow-lg text-lg dark:bg-slate-800 dark:text-slate-100 outline-none transition-colors focus:border-cyan-500',
-    )}
-    {...props}
-  />
-);
+) => <input ref={ref} className={cn(className, styles.input)} {...props} />;
 
 export default forwardRef(Input);
