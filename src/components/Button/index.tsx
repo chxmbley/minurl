6c7efac06552d701f1571fe34ecc579ce8e7b913
Joshua Chumbley <jchumbley@protonmail.com>
2022-01-16 13:27:44 -0600

    Separate Button styles into stylesheet

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,16 +1,11 @@
 import cn from 'classnames';
 import type { FC } from 'react';
 import type { ButtonProps } from './types';
+import styles from './Button.module.scss';
 
 /** Styled Button component */
 const Button: FC<ButtonProps> = ({ className, ...props }) => (
-  <button
-    className={cn(
-      className,
-      'flex justify-center items-center bg-cyan-500 dark:bg-cyan-500 text-white transition-colors hover:bg-cyan-400 focus:bg-cyan-400 outline-none disabled:bg-slate-300 disabled:text-slate-400 disabled:dark:bg-slate-700 disabled:dark:text-slate-500 disabled:cursor-not-allowed',
-    )}
-    {...props}
-  />
+  <button className={cn(className, styles.button)} {...props} />
 );
 
 export default Button;
