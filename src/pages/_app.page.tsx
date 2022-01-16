848dd6eb8920653f9fb5a34a62cd027325776a30
Joshua Chumbley <jchumbley@protonmail.com>
2022-01-16 11:28:12 -0600

    Remove boilerplate ApolloClient code from root App component

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,13 +1,7 @@
-import { ApolloProvider } from '@apollo/client';
-import client from '~lib/api/client';
 import type { FC } from 'react';
 import type { AppProps } from 'next/app';
 import '~styles/globals.scss';
 
-const App: FC<AppProps> = ({ Component, pageProps }) => (
-  <ApolloProvider client={client}>
-    <Component {...pageProps} />
-  </ApolloProvider>
-);
+const App: FC<AppProps> = ({ Component, pageProps }) => <Component {...pageProps} />;
 
 export default App;
