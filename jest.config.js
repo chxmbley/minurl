8adc34b7bfd930029dc7dbe4b7ef811c83c7bc4a
Joshua Chumbley <jchumbley@protonmail.com>
2022-01-17 10:00:35 -0600

    Add coverage requirements to Jest config

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,16 @@ const nextJest = require('next/jest');
 const createJestConfig = nextJest({ dir: './' });
 
 const config = {
+  collectCoverage: true,
+  coverageReporters: ['json', 'html'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 100,
+      lines: 95,
+      statements: 95,
+    },
+  },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: { '^~(.*)$': '<rootDir>/src/$1' },
   testEnvironment: 'jest-environment-jsdom',
