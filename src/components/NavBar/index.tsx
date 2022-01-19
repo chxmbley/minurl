f43978105c1a4b3191e1d575e457a3c019aa1086
Joshua Chumbley <jchumbley@protonmail.com>
2022-01-19 10:23:46 -0600

    Remove unused login/signup buttons and update snapshot

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import Button from '~components/Button';
 import type { FC } from 'react';
 
 /** Application navigation bar */
@@ -10,11 +9,6 @@ const NavBar: FC = () => (
         <h1 className="text-2xl font-bold text-slate-700 dark:text-slate-200">minurl 💎</h1>
       </a>
     </Link>
-
-    <div className="flex">
-      <Button className="mr-2">Log In</Button>
-      <Button variant="primary">Sign Up</Button>
-    </div>
   </nav>
 );
 
